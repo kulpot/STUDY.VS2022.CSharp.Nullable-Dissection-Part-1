3d16ff2f85dcc725efba9ec82a8545fe84329879
Kulpot <kulpotwahu@gmail.com>
2023-08-29 03:23:05 +0800

    Add project files.

---
+++ Nullable Dissection Part 1/JNullable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+struct JNullable<T> where T : struct
+{
+    T value;
+    bool hasValue;
+    public JNullable(T value)
+    {
+        this.value = value;
+        hasValue = true;
+    }
+    public override string ToString()
+    {
+        return value.ToString();
+    }
+}
+
---
+++ Nullable Dissection Part 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=CLpCkitRu_8&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=21
+//
+
+class MainClass
+{
+    static void Main()
+    {
+        int? i = 5;
+        JNullable<int> jamieI =  new JNullable<int>(5);
+        Console.WriteLine(i);
+        Console.WriteLine(jamieI);
+        //Nullable<int> j = 6;
+        //int? sum = i + j;
+        //Console.WriteLine(sum);
+        i = null;
+        jamieI = new JNullable<int>();
+    }
+}
